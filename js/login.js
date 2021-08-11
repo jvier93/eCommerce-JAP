e41a7ac419af622702337f5279b15c445dec9609
Pablo Kraiñuk <pablo-krai@hotmail.com>
2021-08-11 20:06:11 -0300

    form de login con sus respectivas validaciones

--- js/login.js
+++ js/login.js
@@ -1,6 +1,58 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+  //Manejo del focus de los small que muestran los errores
+  document.getElementById("userEmail").addEventListener("focus", (e) => {
+    document.getElementById("userEmailError").textContent = "";
+    //document.getElementById("userEmail").classList.remove("error-input");
+  });
+  document.getElementById("userPassword").addEventListener("focus", (e) => {
+    document.getElementById("userPasswordError").textContent = "";
+    //document.getElementById("userPassword").classList.remove("error-input");
+  });
 
+  //Validamos los inputs cuando se ejecuta el evento blur
+  validateInput("userEmail", "userEmailError", "email", "blur");
+  validateInput("userPassword", "userPasswordError", "password", "blur");
+
+  //Validamos los inputs cuando se ejecuta el evento submit
+  document
+    .getElementById("login-form")
+    .addEventListener("submit", function (e) {
+      if (!this.checkValidity()) {
+        validateInput("userEmail", "userEmailError", "email");
+        validateInput("userPassword", "userPasswordError", "password");
+      }
+
+      e.preventDefault();
+    });
 });
+
+//Valida un input cuando se ejecuta un evento en el mismo (parametro opcional)
+function validateInput(inputId, spanErrorId, inputType, onEvent = null) {
+  if (onEvent !== null) {
+    document.getElementById(inputId).addEventListener(onEvent, function () {
+      if (this.checkValidity()) {
+        this.classList.remove("error-input");
+        document.getElementById(spanErrorId).textContent = "";
+      } else {
+        this.classList.add("error-input");
+        document.getElementById(
+          spanErrorId
+        ).textContent = `${inputType} inválido`;
+      }
+    });
+  } else {
+    const input = document.getElementById(inputId);
+    if (input.checkValidity()) {
+      input.classList.remove("error-input");
+      document.getElementById(spanErrorId).textContent = "";
+    } else {
+      input.classList.add("error-input");
+      document.getElementById(
+        spanErrorId
+      ).textContent = `inserte un ${inputType} válido`;
+    }
+  }
+}
