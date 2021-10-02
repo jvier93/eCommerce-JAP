f9e16144f5525aeebdb901cfdd22a5355222470b
Pablo Kraiñuk <pablo-krai@hotmail.com>
2021-10-02 14:52:54 -0300

    Se modifico el dropdown del navbar y se agrego un Carrousel

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,6 @@
 let commentsArray = [];
+//Variable que indica la posicion del carousell
+let imageActive = 0;
 
 //Funcion especifica para este caso de uso el cual es: a partir de una fecha 2020-02-21 15:05:22 obtener solo 2020-02-21
 function formatDate(dateToFormat) {
@@ -69,7 +71,7 @@ function showRelatedProducts(arrayRelatedProducts, fetchFrom, showInContainer) {
     if (resultObj.status === "ok") {
       const allProducts = resultObj.data;
 
-      arrayRelatedProducts.map((indexOfRelatedProduct) => {
+      arrayRelatedProducts.forEach((indexOfRelatedProduct) => {
         const { name, imgSrc } = allProducts[indexOfRelatedProduct];
         const relatedProduct = createElement({
           type: "div",
@@ -95,6 +97,69 @@ function showRelatedProducts(arrayRelatedProducts, fetchFrom, showInContainer) {
   //showInContainer : donde vamos a mostrar esos productos
 }
 
+function getDate() {
+  const date = new Date();
+  let year = date.getFullYear();
+  //¿Que sucede aca?, Primero le agregamos un 0 al mes obtenido de la variable date y le sumamos 1 al mes ya que se encuentra en base 0
+  //Segundo nos quedamos con los ultimas 2 cifras del month, asi si el mes es menor a 10 de igual forma obtendremos un numero con un 0 adelante.
+  let month = "0" + (date.getMonth() + 1);
+  month = month.slice(-2);
+
+  let day = "0" + date.getDate();
+  day = day.slice(-2);
+  return `${year}-${month}-${day}`;
+}
+
+//Funcion para cargar las imagenes al carrousel
+function putImages(images) {
+  const qtyImages = images.length;
+  //Creamos el div contenedor de las imagenes
+  const imagesWrapper = createElement({
+    type: "div",
+    appendToId: "carrousel",
+    className: "imagesWrapper",
+  });
+  //Añadimos un width de un 100% por cada imagen
+  imagesWrapper.style.width = `${qtyImages * 100}%`;
+
+  //Por cada imagen creamos un img y lo añadimos a nuestro contenedor de imagenes
+  images.forEach((image) => {
+    const img = createElement({
+      type: "img",
+      appendTo: imagesWrapper,
+      imageSrc: image,
+    });
+    //Siendo el contenedor de las imagenes el 100% se divide ese porcentaje por cada imagen
+    img.style.width = `${100 / qtyImages}%`;
+  });
+}
+
+function translate(translateToImage) {
+  const qtyImages = document.querySelectorAll("#carrousel img").length - 1;
+  if (translateToImage > qtyImages) {
+    imageActive = 0;
+  }
+  if (translateToImage < 0) {
+    imageActive = qtyImages;
+  }
+  imagesWrapper = document.querySelector(".imagesWrapper");
+
+  //Trasladamos el elemento en el eje x segun lo que se resuelva en la operacion
+  //para imageActive = 1 y qtyImages + 1 = 3
+  //1 * -(100/3)
+  //1 * -(33,33)
+  //-33,33% (eso quiere decir que desplazamos el elemento hacia la izquierda (-), un 33,33%)
+  //
+  //para imageActive = 2 y qtyImages + 1 = 3
+  //2 * -(100/3)
+  //2 * -(33,33)
+  //66,66% (eso quiere decir que desplazamos el elemento hacia la izquierda (-), un 66,66% )
+
+  imagesWrapper.style.transform = `translateX( ${
+    imageActive * -(100 / (qtyImages + 1))
+  }%)`;
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -118,7 +183,18 @@ document.addEventListener("DOMContentLoaded", function (e) {
       ).textContent = `${currency} ${cost}`;
       document.getElementById("productSoldCount").textContent = soldCount;
       document.getElementById("productCategory").textContent = category;
-      showImagesGallery(images, "productImagesGallery");
+
+      putImages(images);
+      document.getElementById("back").addEventListener("click", (e) => {
+        e.preventDefault();
+        translate((imageActive += -1));
+      });
+
+      document.getElementById("forward").addEventListener("click", (e) => {
+        e.preventDefault();
+        translate((imageActive += 1));
+      });
+
       showRelatedProducts(relatedProducts, PRODUCTS_URL, "relatedProducts");
     }
   });
@@ -129,20 +205,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
       showComments();
     }
   });
-
-  function getDate() {
-    const date = new Date();
-    let year = date.getFullYear();
-    //¿Que sucede aca?, Primero le agregamos un 0 al mes obtenido de la variable date
-    //Segundo nos quedamos con los ultimas 2 cifras del month, asi si el mes es menor a 10 de igual forma obtendremos un numero con un 0 adelante.
-    let month = "0" + date.getMonth();
-    month = month.slice(-2);
-    ////////
-
-    let day = "0" + date.getDay();
-    day = day.slice(-2);
-    return `${year}-${month}-${day}`;
-  }
 
   document
     .getElementById("form-comment-description")
